18cf2d099fff3160ed7306012439de2a9570a193
Victor Fernandes <154020731+VFSomente@users.noreply.github.com>
2025-02-18 16:03:05 -0300

    Remoção back-end

--- Back-end/competicao/competicao/src/main/java/com/example/competicao/repository/TimesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.competicao.repository;
-
-import com.example.competicao.model.Times;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface TimesRepository extends JpaRepository<Times, Long> {
-}
